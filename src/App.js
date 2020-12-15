9c8c5ad0e0c98e3b5670c63bada75188ae2c3ea3
demi zhao <demizhao@demis-MacBook-Pro.local>
2020-12-14 21:23:51 -0500

    test pre commit hooks

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react'
 import { hot } from 'react-hot-loader'
 
 class App extends React.Component {
+  debug
   render() {
     return (
       <div>
-        <h1>Hello World.</h1>
+        <h1>Hello World!</h1>
       </div>
     )
   }
